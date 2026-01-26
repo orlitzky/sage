d443703d0de9d5d24d955a55491b7402043ee148
Frédéric Chapoton <fchapoton2@gmail.com>
2026-01-26 17:12:37 +0100

    fix linter in chute_move.py

--- src/sage/combinat/posets/chute_move.py
+++ src/sage/combinat/posets/chute_move.py
@@ -28,7 +28,7 @@ def intervals_to_polyomino(intervals) -> list[tuple[int, int]]:
     cells as a list of pairs of integers `(x, y)`
 
     EXAMPLES::
-    
+
         sage: intervals_to_polyomino([[0,4],[1,3],[2,2]])
         [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (2, 1), (2, 2), (2, 3), (3, 2)]
 
