f94f10fbc7358a73629ad8cb3d98abdf745f120c
ArixEggink <anouk.eggink@gmail.com>
2026-06-09 17:30:55 -0600

    With the indent

--- src/sage/rings/function_field/drinfeld_modules/drinfeld_module.py
+++ src/sage/rings/function_field/drinfeld_modules/drinfeld_module.py
@@ -48,7 +48,6 @@ from sage.structure.unique_representation import UniqueRepresentation
 
 lazy_import('sage.rings.ring_extension', 'RingExtension_generic')
 
-
 class DrinfeldModule(Parent, UniqueRepresentation):
     r"""
     This class implements Drinfeld `\GF{q}[T]`-modules.
@@ -906,27 +905,27 @@ class DrinfeldModule(Parent, UniqueRepresentation):
         from sage.rings.function_field.drinfeld_modules.action import DrinfeldModuleAction
         return DrinfeldModuleAction(self)
 
-def automorphism_group_order(self, level = False, absolute=False, extension_degree=1):
-    if not absolute:
-        if not self.is_finite():
-            raise ValueError('Drinfeld module must be over a finite field for not absolute automorphism group computations')
-    if absolute:
-        if extension_degree!=1:
-            raise ValueError('Extension degree does nothing on absolute automorphism groups')
-    coef = self.coefficients(sparse=False)
-    level_ = self.rank()
-    for i in range(1,len(coef)):
-        if coef[i] != 0:
-            level_ = gcd(level_,i)
-    q = self.function_ring().base_ring().order()
-    if not absolute:
-        K = self.base()
-        n = log(K.order()**extension_degree, q)
-        level_ = gcd(level_, n)
-    if level:
-        return level_
-    else:
-        return q^level_ - 1
+    def automorphism_group_order(self, level = False, absolute=False, extension_degree=1):
+        if not absolute:
+            if not self.is_finite():
+                raise ValueError('Drinfeld module must be over a finite field for not absolute automorphism group computations')
+        if absolute:
+            if extension_degree!=1:
+                raise ValueError('Extension degree does nothing on absolute automorphism groups')
+        coef = self.coefficients(sparse=False)
+        level_ = self.rank()
+        for i in range(1,len(coef)):
+            if coef[i] != 0:
+                level_ = gcd(level_,i)
+        q = self.function_ring().base_ring().order()
+        if not absolute:
+            K = self.base()
+            n = log(K.order()**extension_degree, q)
+            level_ = gcd(level_, n)
+        if level:
+            return level_
+        else:
+            return q^level_ - 1
 
 
 
