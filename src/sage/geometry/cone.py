994d629d133caa12c1f94d02bc70dd69232c3ad4
Michael Orlitzky <michael@orlitzky.com>
2026-07-16 15:28:00 -0400

    src/sage/geometry/cone.py: remove old workaround in random_cone()
    There is a big comment and a min() in random_cone() that used to work
    around the lack of a concrete max_rays. But now we have max_rays=16 by
    default, and None is not supported. You can still cause yourself
    problems by specifying a huge max_rays, but don't do that.

--- src/sage/geometry/cone.py
+++ src/sage/geometry/cone.py
@@ -6593,25 +6593,8 @@ def random_cone(lattice=None, min_ambient_dim=0, max_ambient_dim=8,
         #
         # Let's begin with an easier question: how many rays should we
         # start with? If we want to attain r rays in this iteration,
-        # then surely r is a good number to start with, even if some
-        # of them will be redundant?
-        #
-        # Not quite, because after 2*d rays, there is a greater
-        # tendency for them to be redundant. If, for example, the
-        # maximum number of rays is unbounded, then r could be enormous
-        # Ultimately that won't be a problem, because almost all of
-        # those rays will be thrown out. However, as we discovered in
-        # Issue #24517, simply generating the random rays in the first
-        # place (and storing them in a list) is problematic.
-        #
-        # Since the returns fall off around 2*d, we start with the
-        # smaller of the two numbers 2*d or r to ensure that we don't
-        # pay a huge performance penalty for things we're going to
-        # throw out anyway. This has a side effect, namely that if you
-        # ask for more than 2*d rays, then you'll probably get the
-        # minimum amount, because we'll start with 2*d and add them
-        # one-at-a-time (see below).
-        rays = [L.random_element() for i in range(min(r,2*d))]
+        # then surely r is a good number to start with?
+        rays = [L.random_element() for _ in range(r)]
 
         # The lattice parameter is required when no rays are given, so
         # we pass it in case r == 0 or d == 0 (or d == 1 but we're
