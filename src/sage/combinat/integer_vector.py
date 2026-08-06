06c69b22e9d13a3636ebee0c339231a9777682f3
vivek41-glitch <vivek41-glitch@users.noreply.github.com>
2026-08-06 13:59:58 +0000

    Remove trailing whitespace

--- src/sage/combinat/integer_vector.py
+++ src/sage/combinat/integer_vector.py
@@ -1659,7 +1659,7 @@ class IntegerVectorsConstraints(IntegerVectors):
         return check_integer_list_constraints(
             x, singleton=True, **self.constraints
         ) is not None
-    
+
     def cardinality(self):
         """
         Return the cardinality of ``self``.
